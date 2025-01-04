cd1785c35ab9dabc136b95b4dd7275b155231c8d
Elkin <elkinrodriguez03@gmail.com>
2025-01-04 13:48:56 -0500

    Add apollo driver for graphql and cors config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'X-Requested-With',
+      'Content-Type',
+      'apollo-requerie-preflight',
+    ],
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  });
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((accumulator, error) => {
+          accumulator[error.property] = Object.values(error.constraints).join(
+            ', ',
+          );
+          return accumulator;
+        }, {});
+        throw new BadRequestException(formattedErrors);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
